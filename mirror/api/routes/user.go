612d08eabac4c77efefe6b059bb2708b90cbf227
Ryan Leckey <ryan@launchbadge.com>
2020-06-28 23:33:22 -0700

    feat: implement GetOtherUsers

--- mirror/api/routes/user.go
+++ mirror/api/routes/user.go
@@ -1,13 +1,29 @@
 package routes
 
 import (
+	"encoding/hex"
 	"github.com/gin-gonic/gin"
+	"github.com/hashgraph/hedera-sdk-go"
+	"github.io/hashgraph/stable-coin/mirror/state"
 	"net/http"
 )
 
 func GetOtherUsersByAddress(c *gin.Context) {
-	// address := c.Param("address")
-	// TODO
+	userNames := make([]string, 0, len(state.User))
 
-	c.JSON(http.StatusOK, []interface{}{})
+	// FIXME: This should be by username
+	hederaPublicKey, err := hedera.Ed25519PublicKeyFromString(c.Param("address"))
+	if err != nil {
+		panic(err)
+	}
+
+	excludeAddress := hex.EncodeToString(hederaPublicKey.Bytes())
+
+	for address, userName := range state.Address {
+		if excludeAddress != address {
+			userNames = append(userNames, userName)
+		}
+	}
+
+	c.JSON(http.StatusOK, userNames)
 }
