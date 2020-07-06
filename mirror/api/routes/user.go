0a1ed9f93fd6461e017ebcf3952c909e24831ec0
Greg Scullard <gregscullard@hedera.com>
2020-07-06 16:12:56 +0200

    Add partial username search

--- mirror/api/routes/user.go
+++ mirror/api/routes/user.go
@@ -6,7 +6,32 @@ import (
 	"github.com/hashgraph/hedera-sdk-go"
 	"github.io/hashgraph/stable-coin/mirror/state"
 	"net/http"
+	"strings"
 )
+
+func GetUsersByPartialMatch(c *gin.Context) {
+	userNames := []string{}
+
+	searchValue := c.Param("username")
+
+	if searchValue != "" {
+
+		state.Address.Range(func(addressI, userNameI interface{}) bool {
+			userName := userNameI.(string)
+
+			if strings.Contains(userName, searchValue) {
+				userNames = append(userNames, userName)
+			}
+
+			return true
+		})
+	}
+	if len(userNames) > 10 {
+		c.JSON(http.StatusOK, userNames[0:10])
+	} else {
+		c.JSON(http.StatusOK, userNames)
+	}
+}
 
 func GetOtherUsersByAddress(c *gin.Context) {
 	userNames := []string{}
