1bba7f6c8c1e67a080a007ef566246db9230ec6f
Greg Scullard <gregscullard@hedera.com>
2020-07-09 18:17:28 +0200

    Get (un)frozen users and check admin status

--- mirror/api/routes/user.go
+++ mirror/api/routes/user.go
@@ -1,11 +1,33 @@
 package routes
 
 import (
+	"encoding/hex"
+	"github.com/hashgraph/hedera-sdk-go"
 	"github.com/labstack/echo/v4"
 	"github.io/hashgraph/stable-coin/mirror/state"
 	"net/http"
 	"strings"
 )
+
+func GetUsersByFrozenStatus(c echo.Context) error {
+	type FrozenUser struct {
+		Name  string   `json:"username"`
+	}
+	var frozen []FrozenUser
+
+	isFrozen := c.Param("frozen")
+
+	state.Frozen.Range(func(userNameI, frozenI interface{}) bool {
+		userName := userNameI.(string)
+
+		if (frozenI.(bool) == (isFrozen == "true")) && userName != "Admin" {
+			frozen = append(frozen, FrozenUser{Name: userName})
+		}
+
+		return true
+	})
+	return c.JSON(http.StatusOK, frozen)
+}
 
 func GetUsersByPartialMatch(c echo.Context) error {
 	userNames := []string{}
@@ -29,6 +51,37 @@ func GetUsersByPartialMatch(c echo.Context) error {
 	} else {
 		return c.JSON(http.StatusOK, userNames)
 	}
+}
+
+func IsAdminUser(c echo.Context) error {
+
+	var adminUserI interface{}
+	var exists bool
+
+	valid := false
+
+	adminHederaPublicKey, err := hedera.Ed25519PublicKeyFromString(c.Param("publicKey"))
+	if err != nil {
+		valid = false
+	} else {
+		adminPublicKeyHex := hex.EncodeToString(adminHederaPublicKey.Bytes())
+		if adminUserI, exists = state.Address.Load(adminPublicKeyHex); exists {
+			// key match, is it admin ?
+			foundUserName := adminUserI.(string)
+			if foundUserName == "Admin" {
+				valid = true
+			} else {
+				valid = false
+			}
+		} else {
+			// key not known
+			valid = false
+		}
+	}
+
+	return c.JSON(http.StatusOK, echo.Map{
+		"valid": valid,
+	})
 }
 
 //func GetOtherUsersByAddress(c echo.Context) error {
